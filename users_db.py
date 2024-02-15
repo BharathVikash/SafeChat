954a7bbd87e5ef05bd116d8129a7b90259ee1bab
Sakthi Prakash K <92044150+SAKTHIPRAKASH28@users.noreply.github.com>
2024-02-15 09:24:53 +0530

    Update users_db.py

--- users_db.py
+++ users_db.py
@@ -1,23 +1,24 @@
-class UsersDB():
+# class UsersDB():
 
-    users_list = [{"user": "Sakthi", "password": "sakthi",
-                           "mobile": '044-45985645'}, {"user": "prakash", "password": "prakash", "mobile": '85497826352'}]
+#     users_list = [{"user": "Sakthi", "password": "sakthi",
+#                            "mobile": '044-45985645'}, {"user": "prakash", "password": "prakash", "mobile": '85497826352'}]
 
-    def read_db(self, user_name: str, password: str):
+#     def read_db(self, user_name: str, password: str):
 
-        for i in self.users_list:
-            if (i["user"] == user_name and i["password"] == password):
-                return True
-        return False
+#         for i in self.users_list:
+#             if (i["user"] == user_name and i["password"] == password):
+#                 return True
+#         return False
 
-    def write_db(self, user_name: str, password: str, mobile: str):
+#     def write_db(self, user_name: str, password: str, mobile: str):
 
-        self.users_list.append(
-            {"user": user_name, "password": password, "mobile": mobile})
+#         self.users_list.append(
+#             {"user": user_name, "password": password, "mobile": mobile})
 
-        return True
+#         return True
 
-    def getNumber(self, user):
-        for i in self.users_list:
-            if i["user"] == user:
-                return i["mobile"]
+#     def getNumber(self, user):
+#         for i in self.users_list:
+#             if i["user"] == user:
+#                 return i["mobile"]
+#DEPRICATED, REFER YOUR FIREBASE FOR USER DATA
